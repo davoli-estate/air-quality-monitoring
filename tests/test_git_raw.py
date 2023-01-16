e075af0adb280dbe70e07f759542086a0a68ce37
marcdavoli <mdavoli@influxdata.com>
2023-01-16 16:45:28 -0500

    feat: update run.py and git_raw loop

--- tests/test_git_raw.py
+++ tests/test_git_raw.py
@@ -1,20 +1,42 @@
-import uos, urequests, secrets
+import uos, urequests, secrets, network, time
 
-print(secrets.git_username)
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect(secrets.wifi_ssid, secrets.wifi_password)
 
-response = urequests.get("http://www.google.com")
+while not wlan.isconnected() and wlan.status() >= 0:
+    print("Waiting to connect:")
+    time.sleep(1)
+    print(wlan.ifconfig())
+
+
+if wlan.status() != 3:
+    raise RuntimeError('network connection failed')
+else:
+    print("Wi-Fi connected")
+    status = wlan.ifconfig()
+    print( 'IP Address = ' + status[0] )
+    print( status )
+
+
 # URL of the RAW file to clone
 script_url = "https://raw.githubusercontent.com/kelseyhightower/nocode/master/Dockerfile"
-response = urequests.get(script_url)
-response.close()
-print(response.content)
 
-f = open("run.py", "w")
-f.write(script_content)
-f.close()
+while True:
 
-print(uos.listdir())
+    # Pull script from URL
+    script_content = urequests.get(script_url).content
+    print(script_content)
 
-f = open("run.py", "r")
-print(f.read())
-f.close()
+    # Write content of script to a file
+    f = open("run.py", "w")
+    f.write(script_content)
+    f.close()
+
+    # Read and print the script
+    f = open("run.py", "r")
+    print(f.read())
+    f.close()
+    time.sleep(2)
+
+    # Run the script
